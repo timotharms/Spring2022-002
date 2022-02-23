27aea9a0f1a5ff393df23fca41bd074dcbbd492c
JeffCord <47281152+JeffCord@users.noreply.github.com>
2022-02-23 10:28:58 -0600

    "Travel message added - Jeffrey Cordes"

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -3,6 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
+				System.out.println("My name is Jeffrey Cordes. I wish to travel to Japan one day !);
 
 	}
 
