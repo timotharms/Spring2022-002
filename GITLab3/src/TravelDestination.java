b845d0c8b203603c6d6c560d847411bfcfa672d0
Benjamin Lindley <benjameslindley@gmail.com>
2022-02-23 15:27:00 -0600

    travel plans for Benjamin Lindley (second try, messed up pull request process first time)

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -4,7 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 				System.out.println("My name is Jeffrey Cordes. I wish to travel to Japan one day !);
-				System.out.println("My name is Benjamin Lindley. I wish to travel to Mars one day.");
+				System.out.println("My name is Benjamin Lindley. I wish to travel to Jupiter one day.");
 
 	}
 
