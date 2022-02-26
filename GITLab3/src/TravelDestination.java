ac054ff6cba05e76344911abcc82534fc65577f8
izzymo34 <81452878+izzymo34@users.noreply.github.com>
2022-02-26 10:53:36 -0600

    Travel destination added

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -3,13 +3,14 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
-				System.out.println("My name is Jeffrey Cordes. I wish to travel to Japan one day !);
+				System.out.println("My name is Jeffrey Cordes. I wish to travel to Japan one day !");
 				System.out.println("My name is Benjamin Lindley. I wish to travel to Jupiter one day.");
 				System.out.println("My name is Garreth Shonaike. I wish to travel to Seoul one day!");
 				System.out.println("My name is Sabrina Ramon. I wish to travel to Japan one day!");
 				System.out.println("My name is Gia German. I wish to travel to Italy one day!");
 				System.out.println("My name is Kaleb Galvan. I wish to travel to Australia one day!");
 				System.out.println("My name is Aldo Martinez. I wish to travel to Rome, Italy one day!");
+				System.out.println("My name is Isabella Montemayor. I wish to travel to Greece one day!");
 	}
 
 }
