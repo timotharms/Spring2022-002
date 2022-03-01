a848b2b515782c2e1f3556e7cba5e02337e8c218
timotharms <100375621+timotharms@users.noreply.github.com>
2022-02-28 23:13:32 -0600

    Update TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -16,6 +16,7 @@ public class TravelDestination {
 				System.out.println("My name is Aidan Chang. I wish to travel to Taiwan one day !");
 				System.out.println("My name is Pete Acevedo. I wish to travel to Mexico one day !");
 				System.out.println("My name is Noah Mesa. I wish to travel to Ohio one day !");
+				System.out.println("My name is Timothy Armstrong. I wish to travel to Tokyo one day !");
 	}
 
 }
